5d141b9d2683a58d56f39edfea0c42a83919e66b
jonas.pommerening <jonas.pommerening@245f6095-c251-0410-a953-ed8d68ba8cc0>
2008-10-04 12:02:07 +0000

    added rtctl, improved build-structure

--- source/client.c
+++ source/client.c
@@ -1,4 +1,19 @@
 #include "client.h"
 
-// Client to connect to the build server.
+#include <stdint.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <time.h>
+
+#include "storage.h"
+#include "database.h"
+
+struct client_info {
+  clientid_t uid;     // unique client identifier
+  pthread_t  thread;  // only if client is associated with a local thread
+  time_t     timeout; // if later than timeout: kick client
+  table_t    files;   // all files the client has (= all file we sent him)
+  struct sockaddr_in host; // the host from which the client connected
+};
 
